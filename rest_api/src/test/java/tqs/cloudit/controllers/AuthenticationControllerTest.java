2bc0847d317be37f8d1306e9bcd42358c72a3627
aspedrosa <aspedrosa@ua.pt>
2019-04-23 14:11:26 +0100

    Update on Jenkinsfile and pom + simple test class

---
+++ rest_api/src/test/java/tqs/cloudit/controllers/AuthenticationControllerTest.java
@@ -0,0 +1,20 @@
+package tqs.cloudit.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ *
+ * @author aspedrosa
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class AuthenticationControllerTest {
+
+    @Test
+    public void login() {
+        // TODO
+    }
+}
