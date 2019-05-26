96f0a51c4d8659070d89cd223c566d3761d4242c
Filipe Pires <filipesnetopires@ua.pt>
2019-05-26 17:50:02 +0100

    Profile page updated; profile.js not fully functional; profile contoller added.

--- rest_api/src/main/resources/static/assets/js/profile.js
+++ rest_api/src/main/resources/static/assets/js/profile.js
@@ -1,9 +1,89 @@
-$("#profile_form").submit(function(event){
-    console.log("Submitted");
+var profile = {};
+
+window.onload = function() {
+    profile["type"] = "tmp";//localStorage.getItem("type");
+    profile["username"] = "tmp";//localStorage.getItem("username");
+    document.getElementById("type").innerHTML = profile["type"];
+    document.getElementById("username").innerHTML = profile["username"];
+};
+
+$("#profile_form").submit(function(event) {
     event.preventDefault();
+
+    // get form content
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var description = document.getElementById("description").value;
+    var current_password = document.getElementById("cur_pwd").value;
+    var new_password = document.getElementById("new_pwd").value;
+    var new_password_confirmation = document.getElementById("new_pwd_conf").value;
+
+    // verify fields
+
+    let data={};
+
+    if(name!==""){
+        data["name"]=name;
+    } else {
+        // warning toast
+    }
+
+    if(email!==""){
+        data["email"]=email;
+    } else {
+        // warning toast
+    }
+
+    if(description!==""){
+        data["description"]=description;
+    }
+
+    if(current_password!==""){
+        //data["current_password"]=current_password;
+        if(new_password!="" && new_password==new_password_confirmation) {
+            data["new_password"]=new_password;
+        } else {
+            // warning toast
+        }
+    }
+
+    // send http request
+
+    console.log(data);
+    $.ajax({
+        url: base_api_url+"/update_profile", // ???
+        type: "POST",
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json",
+        crossDomain:true,
+        headers: {
+            "Authorization": "Basic " + btoa(profile["username"] + ":" + current_password),
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        success: function(data, status, xhr) {
+            if(status!=="success"){
+                alert(JSON.stringify(data));
+            }else{
+                console.log("success: "+data + ", "+status+", "+JSON.stringify(xhr));
+                //localStorage.setItem("profile") = profile;
+                window.location.href= base_api_url+"/profile";
+            }
+        },
+        error: function(data, status, xhr) {
+            alert(JSON.stringify(data));
+        } 
+    });
 });
 
 $("#clear_btn").click(function(event){
-    console.log("Clear");
-    console.log(event);
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("cur_pwd").value = "";
+    document.getElementById("new_pwd").value = "";
+    document.getElementById("new_pwd_conf").value = "";
+    
 });
+
