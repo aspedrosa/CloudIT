dcbf9d59d671847f6da50435fe2a9375235d7651
aspedrosa <aspedrosa@ua.pt>
2019-05-28 00:01:27 +0100

    Organize Cucumber tests + solve username overlapping problems

--- rest_api/src/test/java/tqs/cloudit/services/StepsDefs.java
+++ rest_api/src/test/java/tqs/cloudit/services/StepsDefs.java
@@ -19,10 +19,38 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import tqs.cloudit.domain.rest.User;
 
+/**
+ * Where all the steps of all features are defined here
+ * Each step has referenced on javadoc on what Scenario(s) of which Feature(s) it is used
+ */
 public class StepsDefs extends TestApplication{
-    
+
     private WebDriver driver;
-    private String currentUsername;
+
+    /**
+     * Used to store the username of the last registered user
+     * Login steps use this for the username field
+     * Variable is static because Class is instantiated for each feature
+     */
+    private static String currentUsername;
+
+    /**
+     * Used to create a new current username making
+     *  it different from the last one by incrementing
+     *  the last one
+     * Variable is static because Class is instantiated for each feature
+     */
+    private static int usernameCount = 0;
+
+    /**
+     * Creates a new username different from the previous
+     *  one used so "username already in use" problems
+     *  don't exist
+     */
+    private void changeCurrentUsername() {
+        usernameCount++;
+        currentUsername = "cucumber_tests" + usernameCount;
+    }
 
     public StepsDefs() {
         FirefoxOptions options = new FirefoxOptions();
@@ -31,34 +59,64 @@ public class StepsDefs extends TestApplication{
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver(options);
     }
-    
-    
+
     @Autowired
     private AuthenticationService authenticationService;
-    
-    
+
+    /*
+     * Specific to feature -> User can sign up on the platform (SignUp.feature)
+     */
+
+    /**
+     * User can sign up on the platform (SignUp.feature)
+     *  - User register in on the platform.
+     *  - User fails to register in the platform.
+     *
+     * User can sign in on the platform. (SignIn.feature)
+     *  - User sign in on the platform.
+     *  - User fails sign in.
+     *
+     *  User can learn about the use of the platform through the website. (AboutPlatform.feature)
+     *  - User accesses the platform's about page.
+     *  - User logs in and learns more about the platform.
+     */
     @Given("that I have access to the platform's website,")
     public void openWebsite() {
         driver.get("http://localhost:8080/home");
     }
-    
+
+    /**
+     * User can sign in on the platform. (SignIn.feature)
+     *  - User sign in on the platform.
+     *
+     *  User can learn about the use of the platform through the website. (AboutPlatform.feature)
+     *  - User logs in and learns more about the platform.
+     */
     @When("I login")
     public void Login() throws InterruptedException {
-        System.out.println(authenticationService.register(new User("test", "test", "", "", "", new TreeSet<>())));
+        changeCurrentUsername();
+
+        User newUser = new User(
+            currentUsername,
+            "test",
+            "",
+            currentUsername + "@mail.com",
+            "",
+            new TreeSet<>()
+        );
+        System.out.println(authenticationService.register(newUser));
+
         WebElement username = driver.findElement(By.id("username"));
         WebElement pwd = driver.findElement(By.id("pwd"));
-        username.sendKeys("test");
+        username.sendKeys(currentUsername);
         pwd.sendKeys("test");
         driver.findElement(By.id("submit_button")).click();
-        currentUsername = "test";
     }
 
-    /* see line 99
-    @Then("I should see the welcome page.")
-    public void checkWelcomePage() {
-    }
-    */
-
+    /**
+     * User can sign in on the platform. (SignIn.feature)
+     *  - User fails sign in.
+     */
     @When("I login without filling the form correctly")
     public void BadLogin() {
         WebElement username = driver.findElement(By.id("username"));
@@ -67,66 +125,106 @@ public class StepsDefs extends TestApplication{
         pwd.sendKeys("NonExistingAccount");
         driver.findElement(By.id("submit_button")).click();
     }
-    
+
+    /**
+     * User can sign up on the platform (SignUp.feature)
+     *  - User fails to register in the platform.
+     *
+     * User can sign in on the platform. (SignIn.feature)
+     *  - User fails sign in.
+     */
     @Then("I should be notified about the errors or missing fields.")
-    public void checkErroMessage() {
+    public void checkErrorMessage() {
         new WebDriverWait(driver,10L).until(
                 (WebDriver d) -> d.findElements(By.id("invalid_credentials_message")).size() > 0);
         assertTrue(driver.findElements( By.id("invalid_credentials_message") ).size() > 0);
         assertEquals(driver.findElement( By.id("invalid_credentials_message") ).getText(), "Error! Invalid Credentials.");
         driver.quit();
     }
-    
-    
-    
-    /*SIGNUP TEST*/
-    
+
+    /*
+     * Specific to feature -> User can sign in on the platform. (SignIn.feature)
+     */
+
+    /**
+     * User can sign up on the platform (SignUp.feature)
+     *  - User register in on the platform.
+     */
     @When("I register in the platform")
     public void register() {
+        changeCurrentUsername();
+
         driver.findElement(By.id("sign_up_link")).click();
         WebElement name = driver.findElement(By.id("name"));
         WebElement username = driver.findElement(By.id("username"));
         WebElement email = driver.findElement(By.id("email"));
         WebElement type = driver.findElement(By.id("type"));
         WebElement pwd = driver.findElement(By.id("pwd"));
-        name.sendKeys("test2");
-        username.sendKeys("test2");
-        email.sendKeys("test2@mail.pt");
+        name.sendKeys("test");
+        username.sendKeys(currentUsername);
+        email.sendKeys(currentUsername + "@mail.pt");
         type.sendKeys("freelancer");
-        pwd.sendKeys("test2");
-        currentUsername = "test2";
+        pwd.sendKeys("test");
     }
-    
+
+    /**
+     * User can sign up on the platform (SignUp.feature)
+     *  - User register in on the platform.
+     *  - User fails to register in the platform.
+     */
     @And("I click the submit button")
     public void click(){
         driver.findElement(By.id("signUpButton")).click();
     }
-    
-    
+
+    /**
+     * User can sign up on the platform (SignUp.feature)
+     *  - User register in on the platform.
+     *
+     * User can sign in on the platform. (SignIn.feature)
+     *  - User sign in on the platform.
+     */
     @Then("I should see the welcome page.")
     public void checkWelcomePage() {
         new WebDriverWait(driver,10L).until(
-                (WebDriver d) -> d.findElement(By.id("welcome_title")).getText().toLowerCase().equals(String.format("welcome %s!", currentUsername)));
+                (WebDriver d) ->  {
+                    WebElement welcome_title = driver.findElement(By.id("welcome_title"));
+                    if (welcome_title == null)
+                        return false;
+                    return welcome_title.getText().toLowerCase().equals(String.format("welcome %s!", currentUsername));
+                });
         driver.quit();
     }
-    
-    
+
+    /**
+     * User can sign up on the platform (SignUp.feature)
+     *  - User fails to register in the platform.
+     */
     @When("I register in the platform incorrectly")
     public void badRegister() {
         /*
         Not sending password.
         */
+        changeCurrentUsername();
+
         driver.findElement(By.id("sign_up_link")).click();
         WebElement name = driver.findElement(By.id("name"));
         WebElement username = driver.findElement(By.id("username"));
         WebElement email = driver.findElement(By.id("email"));
         WebElement type = driver.findElement(By.id("type"));
-        name.sendKeys("test2");
-        username.sendKeys("test2");
-        email.sendKeys("test2@mail.pt");
+        name.sendKeys("test");
+        username.sendKeys(currentUsername);
+        email.sendKeys(currentUsername + "@mail.pt");
         type.sendKeys("freelancer");
     }
-    
+
+    /**
+     * User can sign up on the platform (SignUp.feature)
+     *  - User fails to register in the platform.
+     *
+     * User can sign in on the platform. (SignIn.feature)
+     *  - User fails sign in.
+     */
     @Then("I should be notified about the errors or missing fields")
     public void checkErrorRegisterMessage() {
         new WebDriverWait(driver,10L).until((ExpectedCondition<Boolean>) 
@@ -143,7 +241,11 @@ public class StepsDefs extends TestApplication{
                                         }   // catch 
                                     });
     }
-    
+
+    /**
+     * User can sign up on the platform (SignUp.feature)
+     *  - User fails to register in the platform.
+     */
     @And("have the chance to correct my submission.")
     public void canCorrectForm(){
         assertTrue(driver.findElements(By.id("name")).size() > 0);
@@ -155,20 +257,24 @@ public class StepsDefs extends TestApplication{
     }
 
     /*
-     * vvvvvvvvvvvvvvvvvvvvvvvv Learn about user platform vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+     * Specific to feature -> User can learn about the use of the platform through the website. (AboutPlatform.feature)
      */
 
-    /* See line 35
-    @Given("that I have access to the platform's website,")
-    public void openWebsite() {
-    }
-    */
-
+    /**
+     *  User can learn about the use of the platform through the website. (AboutPlatform.feature)
+     *  - User accesses the platform's about page.
+     *  - User logs in and learns more about the platform.
+     */
     @When("I click on the about tab")
     public void clickAbout() {
         driver.findElement(By.id("about")).click();
     }
 
+    /**
+     *  User can learn about the use of the platform through the website. (AboutPlatform.feature)
+     *  - User accesses the platform's about page.
+     *  - User logs in and learns more about the platform.
+     */
     @Then("I should see the about page.")
     public void checkAboutPage() {
         new WebDriverWait(driver,10L).until((ExpectedCondition<Boolean>)
@@ -176,9 +282,4 @@ public class StepsDefs extends TestApplication{
         driver.quit();
     }
 
-    /* See line 40
-    @When("I login")
-    public void Login() {
-    }
-    */
 }
