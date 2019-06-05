b30e49a3ba0eb42a3bb63ac19f5ecb1d398265c6
Filipe Pires <filipesnetopires@ua.pt>
2019-06-05 23:44:26 +0100

    Message Controller changed and Message Service created.

--- rest_api/src/main/java/tqs/cloudit/controllers/MessageController.java
+++ rest_api/src/main/java/tqs/cloudit/controllers/MessageController.java
@@ -1,47 +1,31 @@
 package tqs.cloudit.controllers;
 
 import java.security.Principal;
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import tqs.cloudit.domain.persistance.Message;
-import tqs.cloudit.repositories.MessageRepository;
+import tqs.cloudit.services.MessageService;
 
 @Controller
 public class MessageController {
-
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
     
     @Autowired
-    private MessageRepository messageRepository;
+    private MessageService messageService;
     
     @MessageMapping("/message")
     public void message(Message message, Principal user) throws Exception {
-        String destinationUser =  message.getDestination();
-        message.setOrigin(user.getName());
-        message.setDate(System.currentTimeMillis());
-        
-        messageRepository.save(message);
-        
-        message.setDestination(null);
-        simpMessagingTemplate.convertAndSend("/secured/queue/"+destinationUser, message);
+        messageService.writeMessageByUsername(message, user.getName());
     }
     
     @MessageMapping("/allMessages")
     public void getAll(Message message, Principal user) throws Exception {
-        JSONObject output = new JSONObject();
-        output.put("messages", messageRepository.getMessages(user.getName(), message.getDestination()));
-        simpMessagingTemplate.convertAndSend("/secured/queue/"+user.getName(), output);
+        messageService.getAllMessagesFromConversation(user.getName(), message.getDestination());
     }
     
     @MessageMapping("/contacts")
     public void contacts(Principal user) throws Exception {
-        JSONObject output = new JSONObject();
-        output.put("contacts", messageRepository.getContacts(user.getName()));
-        simpMessagingTemplate.convertAndSend("/secured/queue/"+user.getName(), output);
+        messageService.getContactsFromUser(user.getName());
     }
 
 }
---
+++ rest_api/src/main/java/tqs/cloudit/services/MessageService.java
@@ -0,0 +1,45 @@
+package tqs.cloudit.services;
+
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import tqs.cloudit.domain.persistance.Message;
+import tqs.cloudit.repositories.MessageRepository;
+
+@Service
+public class MessageService {
+    
+    @Autowired
+    private MessageRepository messageRepository;
+    
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    
+    public void getAllMessagesFromConversation(String username, String destination) {
+        JSONObject output = new JSONObject();
+        output.put("messages", messageRepository.getMessages(username, destination));
+        simpMessagingTemplate.convertAndSend("/secured/queue/"+username, output);
+    }
+    
+    public void getContactsFromUser(String username) {
+        JSONObject output = new JSONObject();
+        output.put("contacts", messageRepository.getContacts(username));
+        simpMessagingTemplate.convertAndSend("/secured/queue/"+username, output);
+    }
+    
+    public void writeMessageByUsername(Message message, String username) {
+        String destinationUser =  message.getDestination();
+        message.setOrigin(username);
+        message.setDate(System.currentTimeMillis());
+        
+        messageRepository.save(message);
+        
+        message.setDestination(null);
+        simpMessagingTemplate.convertAndSend("/secured/queue/"+destinationUser, message);
+    }
+    
+    public void updateMessage() {
+        
+    }
+}
